c34f6c321f57f2d0d55703ee60d313d21474b39c
Sam <sam.dacosta@student.manchester.ac.uk>
2017-03-27 15:47:01 +0100

    thresholding done

---
+++ ex3/cwk4.cpp
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <jpeglib.h>
+
+
+/** Read the JPEG image at `filename` as an array of bytes.
+  Data is returned through the out pointers, while the return
+  value indicates success or failure.
+  NOTE: 1) if image is RGB, then the bytes are concatenated in R-G-B order
+        2) `image` should be freed by the user
+ */
+static inline int
+read_JPEG_file(char *filename,
+               int *width, int *height, int *channels, unsigned char *(image[]))
+{
+  FILE *infile;
+  if ((infile = fopen(filename, "rb")) == NULL) {
+    fprintf(stderr, "can't open %s\n", filename);
+    return 0;
+  }
+
+  struct jpeg_error_mgr jerr;
+  struct jpeg_decompress_struct cinfo;
+  cinfo.err = jpeg_std_error(&jerr);
+  jpeg_create_decompress(&cinfo);
+  jpeg_stdio_src(&cinfo, infile);
+  (void) jpeg_read_header(&cinfo, TRUE);
+  (void) jpeg_start_decompress(&cinfo);
+
+  *width = cinfo.output_width, *height = cinfo.output_height;
+  *channels = cinfo.num_components;
+  // printf("width=%d height=%d c=%d\n", *width, *height, *channels);
+  *image = (unsigned char *)malloc(*width * *height * *channels * sizeof(*image));
+  JSAMPROW rowptr[1];
+  int row_stride = *width * *channels;
+
+  while (cinfo.output_scanline < cinfo.output_height) {
+    rowptr[0] = *image + row_stride * cinfo.output_scanline;
+    jpeg_read_scanlines(&cinfo, rowptr, 1);
+  }
+  jpeg_finish_decompress(&cinfo);
+
+  jpeg_destroy_decompress(&cinfo);
+  fclose(infile);
+  return 1;
+}
+
+
+/** Writes the image in the specified file.
+  NOTE: works with Grayscale or RGB modes only (based on number of channels)
+ */
+static inline void
+write_JPEG_file(char *filename, int width, int height, int channels,
+                unsigned char image[], int quality)
+{
+  FILE *outfile;
+  if ((outfile = fopen(filename, "wb")) == NULL) {
+    fprintf(stderr, "can't open %s\n", filename);
+    exit(1);
+  }
+
+  struct jpeg_error_mgr jerr;
+  struct jpeg_compress_struct cinfo;
+  cinfo.err = jpeg_std_error(&jerr);
+  jpeg_create_compress(&cinfo);
+  jpeg_stdio_dest(&cinfo,outfile);
+
+  cinfo.image_width = width;
+  cinfo.image_height = height;
+  cinfo.input_components = channels;
+  cinfo.in_color_space = channels == 1 ? JCS_GRAYSCALE : JCS_RGB;
+  jpeg_set_defaults(&cinfo);
+  jpeg_set_quality(&cinfo, quality, TRUE);
+
+  jpeg_start_compress(&cinfo, TRUE);
+  JSAMPROW rowptr[1];
+  int row_stride = width * channels;
+  while (cinfo.next_scanline < cinfo.image_height) {
+    rowptr[0] = & image[cinfo.next_scanline * row_stride];
+    jpeg_write_scanlines(&cinfo, rowptr, 1);
+  }
+  jpeg_finish_compress(&cinfo);
+
+  fclose(outfile);
+  jpeg_destroy_compress(&cinfo);
+}
+
+void generateHistogram(int *histogram, unsigned char *image, int noPixels) {
+    for (int i = 0; i < 256; i++) 
+        histogram[i] = 0;
+    for (int i = 0; i < noPixels; i++) 
+        histogram[image[i]]++;
+    for (int i = 0; i < 256; i++) {
+     //   printf("%3d: %d\n", i, histogram[i]);
+    }
+}
+
+int getLowestValue(int *histogram) {
+    int i = 0;
+    while (histogram[i] <= 0)
+        i++;
+    return i;
+}
+
+int average(int *histogram, int bottom, int top) {
+    int noPixels = 0;
+    int total = 0;
+
+    for (int i = bottom; i < top; i++) {
+        total += i * histogram[i];
+        noPixels += histogram[i];
+    }
+
+    if (noPixels == 0)
+        return 0;
+
+    return total / noPixels;
+}
+
+
+int calculateThreshold(int *histogram, int width, int height) {
+    int threshold = getLowestValue(histogram);
+    int newVal = -1;
+    while (threshold != newVal) {
+        int lower = average(histogram, 0, threshold);
+        int heigher = average(histogram, threshold, 256);
+        newVal = (lower + heigher) / 2;       
+        threshold++;
+   }
+
+    return threshold - 1;
+}
+
+int main(int argc, char *argv[]){
+  if (argc < 3) {
+    printf("Usage: ./ex1 image_in.jpg image_out.jpg\n");
+    return 1;
+  }
+
+  unsigned char *image;
+  int width, height, channels;
+  read_JPEG_file(argv[1], &width, &height, &channels, &image);
+
+  int histogram[256];
+  generateHistogram(histogram, image, width * height);
+
+  int threshold = calculateThreshold(histogram, width, height);
+  printf("threshold: %d\n", threshold);
+  for (int i = 0; i < width * height; i++)
+    image[i] = image[i] > threshold ? 255 : 0;
+
+  write_JPEG_file(argv[2], width, height, channels, image, 95);
+
+  return 0;
+}
