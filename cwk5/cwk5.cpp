f306162323b5715ea29c94655a4220dcb8701db8
Sam <samuel.dacosta@student.manchester.ac.uk>
2017-04-26 23:20:38 +0100

    I think done cwk5

--- cwk5/cwk5.cpp
+++ cwk5/cwk5.cpp
@@ -107,7 +107,7 @@ bool isRightMostColumn(int i, int width, int height) {
 int convolve(unsigned char* image, int i, int tmplt[3][3], int width, int height) { 
     int sum = 0;
 
-    sum += image[i];
+    //sum += image[i];
     if (!isTopRow(i, width,height)) {  // One pixel up
         sum += image[i - width] * tmplt[0][1];
         
@@ -144,26 +144,30 @@ unsigned char * edge_detect(unsigned char *image, int width,
                                                   int height,
                                                   unsigned char *magImage,
                                                   unsigned char *orientationImage) {
-    int horiz[3][3] = { {-1, -1, -1},
+    int horiz[3][3] = { {-1, -2, -1},
                         {0 ,  0,  0},
-                        {1 ,  1,  1}};
+                        {1 ,  2,  1}};
     int verti[3][3] = { {-1, 0, 1},
-                        {-1, 0, 1},
+                        {-2, 0, 2},
                         {-1, 0, 1}};
     
-    
-    for (int i = 0; i < width * height; i++) {
+    for (int i = width; i < (width-1) * height; i++) {
+
+        if (isLeftMostColumn(i, width, height) || isRightMostColumn(i,width,height))
+            continue;
         int horizontal = convolve(image, i, horiz, width, height);        
         int vertical = convolve(image, i, verti, width, height);
 
+     
         int mag = (int)sqrt(pow(horizontal, 2) + pow(vertical, 2));
-        int ori = 255;
+        int ori = 0;
 
         if (horizontal != 0) {
             ori = (int)(atan(vertical / horizontal) * 255);
-            ori = ori > 255 ? 255 : ori;
+printf("%d\n", ori);
+            ori = ori < 0 ? 0 : ori;
+            ori = ori > 255 ? 255 : ori ;
         } 
-
         magImage[i] = mag > 255 ? 255 : 0;
         orientationImage[i] = ori;
 
@@ -215,14 +219,17 @@ int main(int argc, char *argv[])
   int width, height, channels, i;
   read_JPEG_file(argv[1], &width, &height, &channels, &image);
 
+  unsigned char *medianImage = median(image, width,height);
+
   unsigned char* magImage = (unsigned char *)malloc(sizeof(char) * width * height);
   unsigned char* orientationImage = (unsigned char *)malloc(sizeof(char) * width * height);
+  
   edge_detect(image, width, height, magImage, orientationImage);
 
   write_JPEG_file(argv[2], width, height, channels, magImage, 95);
   write_JPEG_file(argv[3], width, height, channels, orientationImage, 95);
   
-  write_JPEG_file(argv[4], width, height, channels, median(image, width, height), 95);
+  write_JPEG_file(argv[4], width, height, channels, medianImage, 95);
 
   return 0;
 }
